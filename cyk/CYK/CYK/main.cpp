9a4702974cde8d2b8987d7e12446f8b97af6e201
p-adrian05 <adrian16616@gmail.com>
2021-03-03 12:18:49 +0100

    Init

---
+++ cyk/CYK/CYK/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "CYK.cpp"
+int main()
+{
+    CYK cyk("bbcbba");
+    cyk.Run();
+}
--- domol1/ConsoleApplication3/Analyzer.cpp
+++ domol1/ConsoleApplication3/Analyzer.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Analyzer
